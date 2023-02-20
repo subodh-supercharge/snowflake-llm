340a3675a219474d3c5dd9c5e101767523b5da1e
subodh-supercharge <125820747+subodh-supercharge@users.noreply.github.com>
2023-02-20 13:45:44 +0530

    added libs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Demo using Snowflake", page_icon=":bird:"
+    page_title="Demo", page_icon=":bird:"
 )
 st.header("Snowflake Demo")
 st.write(
